2083e6198e0b2902946e02abd3ae5cd4781cf767
Hà Đông <dong.havan@hostingviet.vn>
2023-04-24 15:02:46 +0700

    hvd

---
+++ HaVanDong0186/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HaVanDong0186.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
